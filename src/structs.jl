fd79d94596bf8f87e579372f140f86c228c4bc33
adamferreira <adam.ferreira.dc@gmail.com>
2022-05-18 23:53:06 +0200

    SoapyDict

---
+++ src/structs.jl
@@ -0,0 +1,24 @@
+# Place holder
+function soapykey(v::V)::K where {K,V} end
+
+mutable struct SoapyDict{K,V} <: AbstractDict{K,V}
+    values::Vector{V}
+    map::Dict{K,UInt}
+    
+    function SoapyDict{K,V}(v::Vector{V}) where {K,V}
+        __map = Dict{K,UInt}()
+        for i = 1:length(v)
+            push!(__map, soapykey(v[i]) => i)
+        end
+        return new(v, __map)
+    end
+end
+
+
+soapykey(k::Int)::String = "$(k)"
+
+Base.show(io::IO, d::SoapyDict) = Base.show(io, d.map)
+
+d = SoapyDict{String,Int}([1,2,5,9,8,7,11])
+println(d)
+println(length(d.map))
